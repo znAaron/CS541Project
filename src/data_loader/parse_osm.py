68b79ae00e25c93bd0edb951d0f3cfa252be4241
Zinan Guo <guozinan2000@gmail.com>
2023-03-02 20:46:33 +0000

    road read and combine

---
+++ src/data_loader/parse_osm.py
@@ -0,0 +1,48 @@
+import osmium
+from road_system import *
+
+DATA_FILE = "../../data/osm-sm/west_lafayette.osm"
+road_system = Road_System()
+
+class Data_Processer(osmium.SimpleHandler):
+    def __init__(self):
+        super(Data_Processer, self).__init__()
+
+    def process_road(self, r):
+        r_type = r.tags.get("highway")
+        
+        if r_type in road_types or r_type in linkroad_types:
+            # process name
+            road_name = r.tags.get("name")
+            if r_type in linkroad_types:
+                road_name = r_type + ":" + str(r.id)
+            elif "junction" in r.tags:
+                road_name = "junction" + ":" + str(r.id)
+            elif road_name is None:
+                road_name = "unnamed"
+
+            # process speed
+            road_speedlimit = 0
+            r_speedlimit = r.tags.get("maxspeed")
+            if r_speedlimit is not None:
+                road_speedlimit = int(r_speedlimit[:-4])
+
+            road = Road(r.id, road_name, r.nodes, road_speedlimit)
+
+            if r_type in linkroad_types:
+                road_system.add_linked_road(road)
+            elif "junction" in r.tags:
+                road_system.add_junction_road(road)
+            else:
+                road_system.add_named_road(road)
+
+    def way(self, w):
+        if "highway" in w.tags:
+            self.process_road(w)
+
+processor = Data_Processer()
+processor.apply_file(DATA_FILE)
+
+road_system.combine_named_roads()
+for road in road_system.processed_roads:
+    print(road)
---
+++ src/data_loader/road_system.py
@@ -0,0 +1,86 @@
+road_types = {"motorway", "trunk", "primary", "secondary", "tertiary", "unclassified", "residential"}
+linkroad_types = {"motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link"}
+
+class Road:
+    def __init__(self, id, name, nodes, speedlimit):
+        self.id = id
+        self.name = name
+        self.speedlimit = speedlimit
+
+        self.nodes = []
+
+        if isinstance(nodes[0], int):
+            self.nodes = nodes
+        else:
+            for n in nodes:
+                self.nodes.append(n.ref)
+        self.src = self.nodes[0]
+        self.dst = self.nodes[len(nodes) - 1]
+
+    def __str__(self):
+        return "{}({}), {}mph, [{} -> {}]".format(
+            self.name, str(self.id), str(self.speedlimit), str(self.src), str(self.dst))
+
+class Road_System:
+    def __init__(self):
+        self.named_roads = {}
+        self.link_roads = []
+        self.junction_roads = []
+
+        self.processed_roads = []
+
+    def add_linked_road(self, road):
+        self.link_roads.append(road)
+
+    def add_junction_road(self, road):
+        self.junction_roads.append(road)
+
+    def add_named_road(self, road):
+        if road.name in self.named_roads:
+            self.named_roads[road.name].append(road)
+        else:
+            self.named_roads[road.name] = [road]
+
+    def combine_named_roads(self):
+        for road_list in self.named_roads.values():
+            road_num = len(road_list)
+            
+            combined_list = []
+            visited = [False] * road_num
+            for i in range(road_num):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                curr_road = road_list[i]
+
+                j = i + 1
+                while j < road_num:
+                    if visited[j] or not self.can_combine_roads(curr_road, road_list[j]):
+                        j += 1
+                    else:
+                        visited[j] = True
+                        curr_road = self.combine_roads(curr_road, road_list[j])
+                        j = i + 1
+                
+                combined_list.append(curr_road)
+            self.processed_roads.extend(combined_list)
+
+    def can_combine_roads(self, r1, r2):
+        return r1.src == r2.src \
+            or r1.dst == r2.dst \
+            or r1.src == r2.dst \
+            or r1.dst  == r2.src
+
+    def combine_roads(self, r1, r2):
+        node_list = []
+        if r1.src == r2.src:
+            node_list = r1.nodes[::-1] + r2.nodes[1:]
+        elif r1.dst == r2.dst:
+            node_list = r1.nodes[:-1] + r2.nodes[::-1]
+        elif r1.src == r2.dst:
+            node_list = r2.nodes + r1.nodes[1:]
+        elif r1.dst  == r2.src:
+            node_list = r1.nodes + r2.nodes[1:]
+        
+        combined_road = Road(r1.id, r1.name, node_list, r1.speedlimit)
+        return combined_road
