b7db6c34f19fb1add66f49816351bbfd149671b2
Zinan Guo <guozinan2000@gmail.com>
2023-03-12 11:30:33 -0400

    mysql connection

--- mysql/start-osm-mysql.sh
+++ mysql/start-osm-mysql.sh
@@ -1,1 +1,6 @@
-docker-compose -f osm-mysql.yml up
+# Not necessary the best practice to setup the password
+# change before production
+rm -rf osm-mysql-env.yaml; \
+envsubst < "osm-mysql.yaml" > "osm-mysql-env.yaml"; \
+docker-compose -f osm-mysql-env.yaml up -d; \
+rm -rf osm-mysql-env.yaml
